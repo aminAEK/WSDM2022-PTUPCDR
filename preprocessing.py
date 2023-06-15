2b0a57922401c171fe3fae865665c0f4289453c9
AMINA AIT ELKADI <66838059+aminAEK@users.noreply.github.com>
2023-06-15 17:45:39 +0100

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -14,16 +14,17 @@ class DataPreprocessingMid():
         self.dealing = dealing
     
     def main(self):
-        print('Parsing ' + self.dealing + ' Mid...')
-        re = []
-        with gzip.open(self.root + 'raw/' + self.dealing + '.gz', 'rb') as f:
-            for line in tqdm.tqdm(f, smoothing=0, mininterval=1.0):
-                line = json.loads(line)
-                re.append([line['reviewerID'], line['asin'], line['overall']])
-        re = pd.DataFrame(re, columns=['uid', 'iid', 'y'])
-        print(self.dealing + ' Mid Done.')
-        re.to_csv(self.root + 'mid/' + self.dealing + '.csv', index=0)
-        return re
+      print('Parsing ' + self.dealing + ' Mid...')
+      re = []
+      with zipfile.ZipFile(self.root + 'raw/' + self.dealing + '.zip', 'r') as archive:
+          with archive.open(self.dealing + '.json') as f:
+              for line in tqdm.tqdm(f, smoothing=0, mininterval=1.0):
+                  line = json.loads(line)
+                  re.append([line['reviewerID'], line['asin'], line['overall']])
+      re = pd.DataFrame(re, columns=['uid', 'iid', 'y'])
+      print(self.dealing + ' Mid Done.')
+      re.to_csv(self.root + 'mid/' + self.dealing + '.csv', index=0)
+      return re
 
 
 class DataPreprocessingReady():
